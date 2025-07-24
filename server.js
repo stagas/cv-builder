24dbc4f5f90b53f165d10cc8b3d0890d0c3c0c87
stagas <gstagas@gmail.com>
2025-07-24 19:08:45 +0300

    save

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path')
 const fs = require('fs').promises
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use('*', (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
